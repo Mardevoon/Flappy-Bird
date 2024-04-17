8c1b3d94fb528cf35a66d19475f6dced43154e32
Roberto Souza <marcos-sf@outlook.com>
2024-04-17 15:41:23 -0300

    Initial commit

---
+++ Assets/Scripts/Voa_passarinho.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Voa_passarinho : MonoBehaviour
+{
+    [SerializeField] private float _velocity = 1.5f;
+    [SerializeField] private float _rotationSpeed = 10f;
+
+    private Rigidbody2D _rb;
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            _rb.velocity = Vector2.up * _velocity;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, _rb.velocity.y * _rotationSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        GameManager.instance.GameOver();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Flappy-Bird
+ Desenvolvimento de jogo para estudo.
